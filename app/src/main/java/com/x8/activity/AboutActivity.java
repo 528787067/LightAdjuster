731ebf84c357d0741381dfac87f6a6701bd7d0f2
方块8 <528787067@qq.com>
2016-11-07 13:51:11 +0800

    change

--- app/src/main/java/com/x8/activity/AboutActivity.java
+++ app/src/main/java/com/x8/activity/AboutActivity.java
@@ -3,11 +3,15 @@ package com.x8.activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.x8.utils.PackInfo;
 
 public class AboutActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ((TextView)this.findViewById(R.id.about_text)).setText(String.format(getString(R.string.about_text), PackInfo.getVersionName(getApplicationContext())));
     }
 }
